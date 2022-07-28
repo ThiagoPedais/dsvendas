0782caabf215695aa673aedacdb1ad725e7eb282
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-07-28 13:30:45 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
-
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import DataTable from "./components/DataTable"
 
 function App() {
-  
-
   return (
-    <div>
-      <h1 className="bg-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   )
 }
 
