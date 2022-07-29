0d9978685046385b3d0932bb6ec5c9c3583aaf12
Thiago Pedais <luizthiago20sccp@gmail.com>
2022-07-29 16:36:50 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ReactApexChart from 'react-apexcharts';
 import { SaleSuccess } from '../../types/sale';
 import { round } from '../../utils/format';
